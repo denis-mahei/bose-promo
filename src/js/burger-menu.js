a54569679ff7753080e50e845889525db89e3471
denis-mahei <denmahei@gmail.com>
2025-06-25 23:36:41 +0300

    add tablet menu

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,24 +1,17 @@
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 const menuBtn = document.querySelector('.js-open-menu');
+const menuClose = document.querySelector('.js-close-menu');
 const menu = document.querySelector('.js-menu-container');
 
-menu.addEventListener('transitionend', () => {
-  if (!menu.classList.contains('is-open')) {
-    menu.style.visibility = 'hidden';
-    menu.style.pointerEvents = 'none';
-  }
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('is-open');
+  menuBtn.setAttribute('aria-expanded', 'true');
+  disableBodyScroll(document.body);
 });
 
-menuBtn.addEventListener('click', () => {
-  const expanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
-
-  menuBtn.setAttribute('aria-expanded', !expanded);
-  if (!expanded) {
-    menu.style.visibility = 'visible';
-    menu.style.pointerEvents = 'auto';
-    menu.classList.add('is-open');
-  } else {
-    menu.classList.remove('is-open');
-  }
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('is-open');
+  menuBtn.setAttribute('aria-expanded', 'false');
+  enableBodyScroll(document.body);
 });
