e8dc19b4eb2c8d27b989f29bb564145836b10b48
denis-mahei <denmahei@gmail.com>
2025-06-26 23:03:40 +0300

    guaranty done

---
+++ src/js/in-view.js
@@ -0,0 +1,17 @@
+const animatedElements = document.querySelectorAll('.anim-on-scroll');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+animatedElements.forEach(el => observer.observe(el));
--- src/main.js
+++ src/main.js
@@ -1,19 +1,2 @@
 import './js/burger-menu.js';
-
-const animatedElements = document.querySelectorAll('.anim-on-scroll');
-
-const observer = new IntersectionObserver(
-  entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('in-view');
-        observer.unobserve(entry.target);
-      }
-    });
-  },
-  {
-    threshold: 0.3,
-  }
-);
-
-animatedElements.forEach(el => observer.observe(el));
+import './js/in-view.js';
